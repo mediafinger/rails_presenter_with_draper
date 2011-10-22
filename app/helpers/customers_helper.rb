aa1ce187499d3e71b5ec91d4fbc92f1063861b9c
Andreas Finger <webmaster@mediafinger.com>
2011-10-22 17:39:25 +0200

    Add some helper methods and modified the view

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,2 +1,12 @@
 module CustomersHelper
+
+  def format_phone_number(country, city, fon)
+    "+" + country.to_s + " (0)" + city.to_s + " - " + fon.to_s
+  end
+
+  def twitter_link(name)
+    link_to "@" + name, "http://twitter.com/#!/#{name}"
+  end
+
 end
+
